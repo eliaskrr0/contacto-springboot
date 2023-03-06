7b9a2c842cbe8411d85158d30a04086d06d4e619
Elias <ilyasseessadak@gmail.com>
2023-03-06 23:13:46 +0100

    Arreglando ciertos errores con la backup

--- src/main/java/eliaskrr/contacto/modelo/Contacto.java
+++ src/main/java/eliaskrr/contacto/modelo/Contacto.java
@@ -4,12 +4,14 @@ import lombok.*;
 
 import javax.persistence.*;
 
+
 @NoArgsConstructor@AllArgsConstructor
 @Getter@Setter
 @ToString
 @Entity
 @Table(name = "contacto")
 public class Contacto {
+
     @Id
     @Column(name = "id_contacto", length = 10)
     @GeneratedValue(strategy = GenerationType.AUTO)
